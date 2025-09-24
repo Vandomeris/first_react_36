b10ca925118e6862e9b2edda5737fff11181ff82
Vando <vandomeri@gmail.com>
2025-09-24 11:46:54 +0300

    added quiz direct answer type

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,14 @@
-import { useState } from "react"
-
+import QuizDirectQuestion from "./components/QuizDirectQuestion";
+import QuizSignleAnswer from "./components/QuizSignleAnswer";
 
 export default function App() {
 
-
-
-  const [todos, setTodos] = useState([
-    { id: 2, todo: 'Забрать зарплату', completed: false },
-    { id: 1, todo: 'Купить молоко', completed: false },
-    { id: 3, todo: 'Выгулять собаку', completed: true },
-  ])
-
-  const [newTodo, setNewTodo] = useState('')
-
-  function toggleCompletion(id) {
-
-
-    setTodos(
-
-
-      todos.map(item => {
-
-        if (item.id === id) {
-          item.completed = !item.completed
-        }
-
-        return item
-      })
-
-    )
-  }
-
-  function addTodo(event) {
-    event.preventDefault()
-
-    setTodos(
-      [
-        ...todos,
-        { id: todos.length + 1, todo: newTodo, completed: false }
-      ]
-    )
-
-    setNewTodo('')
-
-  }
-
-
   return (
-
     <div>
-
-      {
-        todos.map((элемент_массива) => (
-          <div className="flex items-center gap-x-5" style={элемент_массива.completed ? { textDecoration: 'line-through' } : {}}>
-            <span>{элемент_массива.id}</span>
-            <input onChange={() => toggleCompletion(элемент_массива.id)} checked={элемент_массива.completed} type="checkbox" />
-            <span>{элемент_массива.todo}</span>
-          </div>
-        ))
-      }
-
-      <form onSubmit={(event) => addTodo(event)}>
-        <input value={newTodo} onInput={(event) => setNewTodo(event.target.value)} type="text" placeholder="Введите новое дело..." />
-        <button>Создать</button>
-      </form>
+      <QuizDirectQuestion question="Кв. Корень из 16" correctAnswer="4" />
+      <QuizSignleAnswer />
 
     </div>
-
   )
 
 }
---
+++ src/components/QuizSignleAnswer.jsx
@@ -0,0 +1,23 @@
+export default function QuizSignleAnswer() {
+    return (
+        <div>
+            <h3>Где я буду служить, когда меня отчислят?</h3>
+
+            <div>
+                <label>
+                    <input type="radio" name="answer" />
+                    Рязань ВДВ
+                </label>
+                <label>
+                    <input type="radio" name="answer" />
+                    Стройбат
+                </label>
+                <label>
+                    <input type="radio" name="answer" />
+                    Под Ростовом
+                </label>
+            </div>
+            <p></p>
+        </div>
+    )
+}
